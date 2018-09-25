54ca0dec6803d678a195a0f0af5655af1ff06bfd
anliex <admin@anliex.com>
2018-09-25 11:56:14 +0800

    Initial commit

---
+++ src/Flexlive.CQP.CSharpPlugins.Demo/FormSettings.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Flexlive.CQP.CSharpPlugins.Demo
+{
+    partial class FormSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.nud = new System.Windows.Forms.NumericUpDown();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.nud2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(214, 135);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(295, 135);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "退出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // nud
+            // 
+            this.nud.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.nud.Location = new System.Drawing.Point(163, 37);
+            this.nud.Name = "nud";
+            this.nud.Size = new System.Drawing.Size(56, 29);
+            this.nud.TabIndex = 3;
+            this.nud.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.nud.ValueChanged += new System.EventHandler(this.nud_ValueChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 37);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(145, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.TickFrequency = 5;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(20, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "复读概率";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkBox1.Location = new System.Drawing.Point(21, 90);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(232, 25);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "在               秒内只触发一次";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // nud2
+            // 
+            this.nud2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.nud2.Location = new System.Drawing.Point(60, 88);
+            this.nud2.Maximum = new decimal(new int[] {
+            3600,
+            0,
+            0,
+            0});
+            this.nud2.Name = "nud2";
+            this.nud2.Size = new System.Drawing.Size(69, 29);
+            this.nud2.TabIndex = 3;
+            this.nud2.Value = new decimal(new int[] {
+            3600,
+            0,
+            0,
+            0});
+            this.nud2.ValueChanged += new System.EventHandler(this.nud2_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(38, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "触发CD剩余0秒。";
+            // 
+            // FormSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 164);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.nud2);
+            this.Controls.Add(this.nud);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.checkBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormSettings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flexlive.CQP.CSharpPlugins.Demo 插件设置";
+            this.Load += new System.EventHandler(this.FormSettings_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.NumericUpDown nud;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown nud2;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ src/Flexlive.CQP.CSharpPlugins.Demo/FormSettings.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Flexlive.CQP.Framework;
+using System.Data.SqlClient;
+
+namespace Flexlive.CQP.CSharpPlugins.Demo
+{
+    public partial class FormSettings : Form
+    {
+        public FormSettings()
+        {
+            InitializeComponent();
+
+            //加载标题。
+            this.Text = System.Reflection.Assembly.GetAssembly(this.GetType()).GetName().Name + " 参数设置";
+        }
+
+        /// <summary>
+        /// 退出按钮事件处理方法。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// 保存按钮事件处理方法。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //参数保存处理代码。
+            MyPlugin.Chance = Convert.ToInt32(nud.Value);
+            MyPlugin.Time = Convert.ToInt32(nud2.Value);
+            MyPlugin._Time = checkBox1.Checked;
+
+            this.btnExit_Click(null, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SQL conn = new SQL();
+            //conn.Open();
+            //string sql = "create table minzu(xuhao int auto_increment primary key,  minzu varchar(50) not null);";
+            //conn.DoCommand(sql);
+            //conn.Close();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            nud.Value = trackBar1.Value;
+        }
+
+        private void nud_ValueChanged(object sender, EventArgs e)
+        {
+            trackBar1.Value = Convert.ToInt32(nud.Value);
+        }
+
+        private void nud2_ValueChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void FormSettings_Load(object sender, EventArgs e)
+        {
+            nud.Value = MyPlugin.Chance;
+            nud2.Value = MyPlugin.Time;
+            checkBox1.Checked = MyPlugin._Time;
+            label2.Text = "触发CD剩余" + MyPlugin.tempTime + "秒。";
+        }
+    }
+}
